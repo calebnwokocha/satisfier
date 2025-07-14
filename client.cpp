2e3fd968a86754a9364505ed90e8046f3cd97cfc
Caleb Nwokocha <47554663+calebnwokocha@users.noreply.github.com>
2025-07-14 17:25:19 -0500

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -2,17 +2,13 @@
 #include "satisfier.hpp"
 
 int main() {
-   // Propositions:
-   //   Socrates_is:       true
-   //   Socrates_is_man:    true
-   //   All_men_are_mortal: true
+   // Propositions
    suppose_literal(Socrates_is, true);
    suppose_literal(Socrates_is_man,    false);
    suppose_literal(All_men_are_mortal, false);
    //suppose_literal(n1, true);
 
-   // Encode implication: Socrates_is_man → All_men_are_mortal
-   // as ¬Socrates_is_man ∨ All_men_are_mortal
+   // Encode implication
    auto implication = Not(Socrates_is.Or(Socrates_is_man)).Or(Not(All_men_are_mortal));
 
    // Output
