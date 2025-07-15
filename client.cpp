0f61a247d276940922b4ebcce1e654d4b0957f0d
Caleb Nwokocha <47554663+calebnwokocha@users.noreply.github.com>
2025-07-15 07:32:16 -0500

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -7,8 +7,8 @@ int main() {
    suppose_literal(Socrates_is_man,    false);
    suppose_literal(All_men_are_mortal, false);
 
-   // Encode implication
-   auto implication = Not(Socrates_is.Or(Socrates_is_man)).
+   // Assertion
+   auto assertion = Not(Socrates_is.Or(Socrates_is_man)).
                       Or(Not(All_men_are_mortal)).
                       And(Socrates_is);
 
@@ -18,8 +18,8 @@ int main() {
        << (Socrates_is_man.value()    ? "true\n" : "false\n")
        << "All men are mortal:   "
        << (All_men_are_mortal.value() ? "true\n" : "false\n")
-       << "Implication holds:    "
-       << (implication.value()        ? "true\n" : "false\n");
+       << "Assertion holds:    "
+       << (assertion.value()        ? "true\n" : "false\n");
 
    return 0;
 }
