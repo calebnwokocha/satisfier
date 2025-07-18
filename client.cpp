1d2c6002158177cbf3c7cf904354c70e8a54f4c5
Caleb Nwokocha <47554663+calebnwokocha@users.noreply.github.com>
2025-07-18 14:04:37 -0500

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 #include "satisfier.hpp"
 
-int main() {
+int main () 
+{
    // Propositions
-   suppose_literal(Socrates_is, true);
-   suppose_literal(Socrates_is_man,    false);
-   suppose_literal(All_men_are_mortal, false);
+   suppose_literal (Socrates_is, true);
+   suppose_literal (Socrates_is_man,    false);
+   suppose_literal (All_men_are_mortal, false);
 
    // Assertion
-   auto assertion = Not(Socrates_is.Or(Socrates_is_man)).
-                      Or(Not(All_men_are_mortal)).
-                      And(Socrates_is);
+   auto assertion = Not (Socrates_is. Or (Socrates_is_man)).
+                      Or (Not (All_men_are_mortal)).
+                      And (Socrates_is);
 
    // Output
    std::cout
        << "Socrates is a man:    "
-       << (Socrates_is_man.value()    ? "true\n" : "false\n")
+       << (Socrates_is_man. value()    ? "true\n" : "false\n")
        << "All men are mortal:   "
-       << (All_men_are_mortal.value() ? "true\n" : "false\n")
+       << (All_men_are_mortal. value() ? "true\n" : "false\n")
        << "Assertion holds:    "
-       << (assertion.value()        ? "true\n" : "false\n");
+       << (assertion. value()        ? "true\n" : "false\n");
 
    return 0;
 }
